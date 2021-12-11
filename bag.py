3a1a889f252c4d4e1ee4dfad336c02d34c6df12b
xuj2 <72422668+xuj2@users.noreply.github.com>
2021-12-11 15:41:41 -0800

    Update and rename bag_da.py to bag.py

--- bag.py
+++ bag.py
@@ -1,10 +1,9 @@
 from dynamic_array import *
 
+
 class Bag:
     def __init__(self, start_bag=None):
         self.da = DynamicArray()
-
-        # populate bag with initial values
         if start_bag is not None:
             for value in start_bag:
                 self.add(value)
@@ -52,51 +51,3 @@ class Bag:
             if check == False:
                     return False
         return True
-
-
-if __name__ == "__main__":
-
-    print("\n# add example 1")
-    bag = Bag()
-    print(bag)
-    values = [10, 20, 30, 10, 20, 30]
-    for value in values:
-        bag.add(value)
-    print(bag)
-
-    print("\n# remove example 1")
-    bag = Bag([1, 2, 3, 1, 2, 3, 1, 2, 3])
-    print(bag)
-    print(bag.remove(7), bag)
-    print(bag.remove(3), bag)
-    print(bag.remove(3), bag)
-    print(bag.remove(3), bag)
-    print(bag.remove(3), bag)
-
-    print("\n# count example 1")
-    bag = Bag([1, 2, 3, 1, 2, 2])
-    print(bag, bag.count(1), bag.count(2), bag.count(3), bag.count(4))
-
-    print("\n# clear example 1")
-    bag = Bag([1, 2, 3, 1, 2, 3])
-    print(bag)
-    bag.clear()
-    print(bag)
-
-    print("\n# equal example 1")
-    bag1 = Bag([10, 20, 30, 40, 50, 60])
-    bag2 = Bag([60, 50, 40, 30, 20, 10])
-    bag3 = Bag([10, 20, 30, 40, 50])
-    bag_empty = Bag()
-
-    print(bag1, bag2, bag3, bag_empty, sep="\n")
-    print(bag1.equal(bag2), bag2.equal(bag1))
-    print(bag1.equal(bag3), bag3.equal(bag1))
-    print(bag2.equal(bag3), bag3.equal(bag2))
-    print(bag1.equal(bag_empty), bag_empty.equal(bag1))
-    print(bag_empty.equal(bag_empty))
-    print(bag1, bag2, bag3, bag_empty, sep="\n")
-
-    bag1 = Bag([100, 200, 300, 200])
-    bag2 = Bag([100, 200, 30, 100])
-    print(bag1.equal(bag2))
